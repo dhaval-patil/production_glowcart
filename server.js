b9fc6698dcbdca849794b9d6a1eae9be73b26c92
Dhaval Patil <117525111+dhaval-patil@users.noreply.github.com>
2025-05-03 01:13:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
-
+import { fileURLToPath } from 'url';
 // configure
 dotenv.config();
 
 // database config
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest Object
 const app = express();
 
